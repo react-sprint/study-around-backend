c8117d4efc99298409dcd950fff512a73d8a5953
Jaeuk-YOO <ryou9305@naver.com>
2020-11-25 03:04:50 +0900

    hotfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:react-sprint/study-around-backend.git"
 set :deploy_to, "/home/study-around-user/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
+append :linked_files, 'config/credentials/master.key'
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 # Default branch is :master
